eecaa571c3da4a5ddbf6f123fe4a2f4f4ac5a4e1
Arjunkumar1877 <arjun.tech177@gmail.com>
2024-10-20 22:17:26 +0530

    kuma ui installed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
+import { withKumaUI } from "@kuma-ui/next-plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default   withKumaUI(nextConfig);
